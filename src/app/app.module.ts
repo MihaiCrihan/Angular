526849b12182d892bcaa63ea3f96d623637659bf
Cronix <haos-sah@mail.ru>
2019-05-13 19:26:45 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectsComponent } from './selects/selects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectsComponent
   ],
   imports: [
     BrowserModule,
